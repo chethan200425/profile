e94c0c616e6a4e6e12eb1869e6555167d21e6a56
chethan kumar v <117809376+chethan200425@users.noreply.github.com>
2024-12-11 06:36:23 +0530

    Add files via upload

---
+++ profile/script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('contactForm');
+
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
+
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        const message = document.getElementById('message').value;
+
+        if (name === "" || email === "" || message === "") {
+            alert("Please fill out all fields.");
+        } else {
+        
+        }
+    });
+});
